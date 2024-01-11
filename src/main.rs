ae57a866421872d8a2f8baa316491834e1cdd348
Oliver Ellis <ellis.oliver@gmail.com>
2024-01-11 14:44:35 +0100

    Created icon pairs and randomized order

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use slint::Model;
+use rand::seq::SliceRandom;
+
 fn main() {
-   MainWindow::new().unwrap().run().unwrap();
+    let main_window = MainWindow::new().unwrap();
+
+    // Fetch the tiles from the model
+    let mut tiles: Vec<TileData> = main_window.get_memory_tiles().iter().collect();
+
+    // Duplicate them to ensure that we have pairs
+    tiles.extend(tiles.clone());
+
+    // Randomly mix the tiles
+    let mut rng = rand::thread_rng();
+    tiles.shuffle(&mut rng);
+
+    // Assign the shuffled Vec to the model property
+    let tiles_model = std::rc::Rc::new(slint::VecModel::from(tiles));
+    main_window.set_memory_tiles(tiles_model.into());
+
+    main_window.run().unwrap();
 }
 
 slint::slint! {
